319d1549c58a23d20652c8822ed187109954761c
zaidsultan10 <zaidsultan09@gmail.com>
2024-12-25 22:19:44 +0400

    deleted unnecssary things

--- easy-generator-server/src/config/database/mongodb.config.ts
+++
@@ -1,9 +0,0 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-export const databaseConfig: TypeOrmModuleOptions = {
-  type: 'mongodb',
-  url: 'mongodb+srv://dbUser:abubakr@cluster0.8wqhx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  entities: [],
-  logging: true,
-  synchronize: true,
-};
